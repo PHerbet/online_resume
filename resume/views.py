81b9c6b0ee51839d531dc412aaed675a2cddf210
Priscillien Herbet <priscillien.herbet@gmail.com>
2023-03-31 11:07:39 +0200

    fix some issues with slugfield ans add some stuff to views.py

--- resume/views.py
+++ resume/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
 from .models import *
 
 def index(request):
     experiences = Experience.objects.all()
-    context = {'experiences' : experiences}
+    type_competences = Type_competence.objects.all()
+    competences = Competence.objects.all()
+    educations = Education.objects.all()
+    realisation = Realisation.objects.all()
+    ressources = Ressource.objects.all()
+    context = {
+        'type_competences' : type_competences,
+        'experiences' : experiences,
+        'competences' : competences,
+        'educations' : educations,
+        'realisations' : realisation,
+        'ressources' : ressources 
+        }
     return render(request, 'resume/index.html', context)
-
-
