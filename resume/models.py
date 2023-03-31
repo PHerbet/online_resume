66df220c2464bbd87a04137e6624b5fa34777876
Priscillien Herbet <priscillien.herbet@gmail.com>
2023-03-31 10:49:54 +0200

    Revert "change url textfield into slugfield and add some stuff to context views"
    This reverts commit 0b62286f3d12cde8ede4a868579ba9f8333638fc.

--- resume/models.py
+++ resume/models.py
@@ -12,17 +12,13 @@ class Experience(models.Model):
         blank=True, 
         null=True,
         )
-    
+
     def __str__(self):
         return self.position
 
-
+    
 class Type_competence(models.Model):
     name_type = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return self.name_type
-
 
 class Competence(models.Model):
     name_competence = models.CharField(max_length=50)
@@ -37,38 +33,25 @@ class Competence(models.Model):
         null=True,
         )
 
-    def __str__(self):
-        return self.name_competence
-
-
 class Education(models.Model):
     title = models.CharField(max_length=25)
     obtention_year = models.CharField(max_length=10)
 
-    def __str__(self):
-        return self.title
-
-
 class Realisation(models.Model):
     name_realisation = models.CharField(max_length=25)
     description = models.TextField()
-    git_link = models.SlugField(max_length=200)
+    git_link = models.CharField(max_length=200)
     icon_realisation = models.ImageField(
         upload_to="icon_realisation", 
         blank=True, 
         null=True,
         )
-    def __str__(self):
-        return self.name_realisation
-
 
 class Ressource(models.Model):
-    name_ressource = models.CharField(max_length=20)
-    url_ressource = models.SlugField(max_length=200)
+    name_resssource = models.CharField(max_length=20)
+    url_ressource = models.CharField(max_length=100)
     icon_ressource = models.ImageField(
         upload_to="icon_ressource", 
         blank=True, 
         null=True,
         )
-    def __str__(self):
-        return self.name_ressource
--- resume/views.py
+++ resume/views.py
@@ -4,17 +4,7 @@ from .models import *
 
 def index(request):
     experiences = Experience.objects.all()
-    type_competences = Type_competence.objects.all()
-    competences = Competence.objects.all()
-    educations = Education.objects.all()
-    realisation = Realisation.objects.all()
-    ressources = Ressource.objects.all()
-    context = {
-        'type_competences' : type_competences,
-        'experiences' : experiences,
-        'competences' : competences,
-        'educations' : educations,
-        'realisations' : realisation,
-        'ressources' : ressources 
-        }
+    context = {'experiences' : experiences}
     return render(request, 'resume/index.html', context)
+
+
